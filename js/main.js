698350ee1423961cbeb26320f69cc9d44c97fa68
Freyze10 <bjabillanoza@gmail.com>
2025-07-03 22:37:38 +0800

    responsive

---
+++ js/main.js
@@ -0,0 +1,6 @@
+function openNav() {
+    document.querySelector("header nav").style.bottom = 'calc(100% - 240px)';
+}
+function closeNav() {
+    document.querySelector("header nav").style.bottom = '100%';
+}
